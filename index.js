2f5fa7350e3807f4f776cccb82c3698394793238
Kunal Khatri <87149893+kunkhatri@users.noreply.github.com>
2021-12-17 13:56:10 +0530

    change in property names of stacktrace object and identifying internal calls in stack trace

--- index.js
+++ index.js
@@ -1,24 +1,60 @@
 const StackUtils = require('stack-utils');
 
 function _defaultCallSiteParser(callSite) {
-    const isInternal = callSite.getFileName().includes('internal/');
+    const functionName = callSite.getFunctionName(), methodName = callSite.getMethodName();
+    const possibleFunctionName = functionName != null ? functionName : (methodName != null ? methodName : '<unknown>'); 
     return {
-        function: callSite.getFunctionName(),
-        method: callSite.getMethodName(),
-        filename: callSite.getFileName(),
-        lineno: callSite.getLineNumber(),
-        colno: callSite.getColumnNumber(),
-        in_app: !isInternal,
+        function_name: possibleFunctionName,
+        file_name: callSite.getFileName(),
+        line_number: callSite.getLineNumber(),
+        column_number: callSite.getColumnNumber(),
     };
 }
 
-function captureAndParseStackTrace({ limit = 20, startStackFunction = captureAndParseStackTrace, callSiteParser = _defaultCallSiteParser } = {}) {
+function _classifyInternalCallSites(parsedCallSites) {
+    const internalModules = StackUtils.nodeInternals();
+    let updatedCallSites = [];
+    parsedCallSites.forEach(callSite => {
+        const fileName = callSite.file_name.replace(/\\/g, '/');
+        if (internalModules.some(internal => {
+            let stackLine = fileName + ':' + callSite.line_number + ':' + callSite.column_number;
+            if (internal.toString().startsWith('/\\(') && internal.toString().endsWith('\\)$/')) {
+                stackLine = '(' + stackLine + ')';
+            }
+            return internal.test(stackLine);
+        })) {
+            updatedCallSites.push(Object.assign(callSite, { in_app: false }));
+        } else {
+            updatedCallSites.push(Object.assign(callSite, { in_app: true }));
+        }
+    });
+    return updatedCallSites;
+}
+
+/**
+ * Captures and parses call sites in stack trace to generate stack trace
+ * in structured format, rather than string.
+ * 
+ * @param {number} limit - limits the number of calls in stack trace.
+ * @param {Function} startStackFunction - function where the stack trace should start from. first line will be the
+ * function that calls startStackFunction.
+ * @param {Function} callSiteParser - function to parse CallSite objects from a stack trace.
+ * @returns {*} - parsed call sites.
+ */
+
+function captureAndParseStackTrace({ 
+    limit = 20, 
+    startStackFunction = captureAndParseStackTrace, 
+    callSiteParser = _defaultCallSiteParser 
+} = {}) {
     const stackUtils = new StackUtils({ wrapCallSite: callSiteParser });
     const parsedCallSites = stackUtils.capture(limit, startStackFunction);
 
-    return {
-        frames: parsedCallSites,
-    };
+    if (callSiteParser === _defaultCallSiteParser) {
+        return { frames: _classifyInternalCallSites(parsedCallSites) };
+    }
+
+    return { frames: parsedCallSites };
 }
 
 module.exports = {
